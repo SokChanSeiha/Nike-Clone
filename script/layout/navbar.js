d7fc37121b14e973eb7a831d41236ebfffd52093
SokChanSeiha <sokchanseiha@gmail.com>
2024-05-08 16:22:16 +0700

    add page: start building kids page

--- script/layout/navbar.js
+++ script/layout/navbar.js
@@ -4,28 +4,5 @@ fetch("../../components/layout/navbar.html")
   .then((data) => {
     navbar.innerHTML = data;
 
-    // side bar
-    const minMenuDropdown = document.querySelector("#minMenuDropdown");
-    // open and close sidebar
-    const menuButton = document.getElementById("menuButton");
-
-    menuButton.addEventListener("click", () => {
-      const menuIcon = document.getElementById("menuIcon");
-      // Toggle the 'active' class on the menu button
-      menuButton.classList.toggle("active");
-      // Toggle between menu icon and close icon
-      if (menuButton.classList.contains("active")) {
-        menuIcon.innerHTML =
-          '<img  src="../../images/layout/close.png" alt="Close">'; // Set close icon HTML
-        minMenuDropdown.style.right = "0%";
-      } else {
-        menuIcon.innerHTML =
-          '<img  src="../../images/layout/menu.png" alt="Menu">'; // Set menu icon HTML
-        minMenuDropdown.style.right = "-100%";
-      }
-    });
-
-
-    //
   })
   .catch((error) => console.error("Error fetching included file:", error));
