ebbfea12e94d01d6b4f2ec394ddd75166203a17d
SokChanSeiha <sokchanseiha@gmail.com>
2024-06-12 14:57:48 +0700

    file: edit sematic element in women page

--- src/pages/women/women.js
+++ src/pages/women/women.js
@@ -5,13 +5,13 @@ const createWomenPage = () => {
     <div class="max-w-[1920px] mx-auto w-full lg:px-10 px-5 flex flex-col justify-center">
 
         <!-- Image of hero section -->
-        <div>
+        <picture>
             <img src="../../images/women/bigImgLG.png" class="hidden sm:block" alt="">
             <img src="../../images/women/bigImgSM.png" class="block sm:hidden w-full" alt="">
-        </div>
+        </picture>
 
         <!-- Description of hero section-->
-        <div class="sm:mx-auto max-w-7xl flex flex-col gap-4 py-8">
+        <article class="sm:mx-auto max-w-7xl flex flex-col gap-4 py-8">
             <h1 class="sm:mx-auto font-medium">Nike Indy Bra</h1>
             <h1 class="sm:mx-auto text-5xl lg:text-7xl font-black tracking-[-2px] oswald">FEEL SCULPTED</h1>
             <h1 class="sm:mx-auto">Light, medium or high support fo every move, for every body.</h1>
@@ -21,7 +21,7 @@ const createWomenPage = () => {
                     <h1 class=" text-white">Shop</h1>
                 </button>
             </div>
-        </div>
+        </article>
     </div>
     
     <!-- Shop the Essential's content -->
@@ -45,31 +45,31 @@ const createWomenPage = () => {
         <div class="flex gap-6 mb-6 overflow-x-scroll md:px-12 px-6">
             <!-- 1st image card -->
             <div class="w-full">
-                <div class="sm:w-[500px] w-[400px] xl:w-[600px]">
+                <article class="sm:w-[500px] w-[400px] xl:w-[600px]">
                     <a href=""><img class="w-full" src="../../images/women/shop-1.png" alt=""></a>
                     <h1 class="text-lg my-3 mb-9">Clothing</h1>
-                </div>
+                </article>
             </div>
             <!-- 2nd image card -->
             <div class="w-full">
-                <div class="sm:w-[500px] w-[400px] xl:w-[600px]">
+                <article class="sm:w-[500px] w-[400px] xl:w-[600px]">
                     <a href=""><img class="w-full" src="../../images/women/shop-2.png" alt=""></a>
                     <h1 class="text-lg my-3 mb-9">Shoes</h1>
-                </div>
+                </article>
             </div>
             <!-- 3rd image card -->
             <div class="w-full">
-                <div class="sm:w-[500px] w-[400px] xl:w-[600px]">
+                <article class="sm:w-[500px] w-[400px] xl:w-[600px]">
                     <a href=""><img class="w-full" src="../../images/women/shop-3.png" alt=""></a>
                     <h1 class="text-lg my-3 mb-9">Accessories</h1>
-                </div>
+                </article>
             </div>
             <!-- 4th image card -->
             <div class="w-full">
-                <div class="sm:w-[500px] w-[400px] xl:w-[600px]">
+                <article class="sm:w-[500px] w-[400px] xl:w-[600px]">
                     <a href=""><img class="w-full" src="../../images/women/shop-4.png" alt=""></a>
                     <h1 class="text-lg my-3 mb-9">Bras & Leggings</h1>
-                </div>
+                </article>
             </div>
         </div>
     </div>
@@ -80,40 +80,41 @@ const createWomenPage = () => {
             <h1 class="text-2xl font-medium tracking-[0.5px]">Trending</h1>
         </div>
         <div class="max-w-[1920px] flex flex-col mx-auto text-white md:px-12 px-6 sm:flex-row">
+
             <!-- picture1 -->
             <div class="relative w-full md:block hidden">
                 <a class=""><img class="w-full h-full object-cover " src="../../images/women/womenLG.png" alt=""></a>
-                <div class="absolute left-11 bottom-11">
+                <article class="absolute left-11 bottom-11">
                     <h1>Just In</h1>
                     <h1 class="text-lg mt-1 tracking-wide">Air Max DN</h1>
                     <button class="bg-white py-1.5 px-5 rounded-full mt-8 text-black font-medium">Shop</button>
-                </div>
+                </article>
             </div>
             <div class="relative w-full md:hidden block">
                 <a class=""><img class="w-full h-full object-cover " src="../../images/women/womenSM.png" alt=""></a>
-                <div class="absolute left-11 bottom-11">
+                <article class="absolute left-11 bottom-11">
                     <h1>Just In</h1>
                     <h1 class="text-lg mt-1 tracking-wide">Air Max DN</h1>
                     <button class="bg-white py-1.5 px-5 rounded-full mt-8 text-black font-medium">Shop</button>
-                </div>
+                </article>
             </div>
     
             <!-- picture2 -->
             <div class="relative w-full md:block hidden">
                 <a class=""><img class="w-full h-full" src="../../images/home/f-women-lg.png" alt=""></a>
-                <div class="absolute left-11 bottom-11">
+                <article class="absolute left-11 bottom-11">
                     <h1>Make Your Move</h1>
                     <h1 class="text-lg mt-1 tracking-wide">New Fitness Essential</h1>
                     <button class="bg-white py-1.5 px-5 rounded-full mt-8 text-black font-medium">Shop</button>
-                </div>
+                </article>
             </div>
             <div class="relative w-full md:hidden block">
                 <a class=""><img class="w-full h-full" src="../../images/women/girlsSM.png" alt=""></a>
-                <div class="absolute left-11 bottom-11">
+                <article class="absolute left-11 bottom-11">
                     <h1>Make Your Move</h1>
                     <h1 class="text-lg mt-1 tracking-wide">New Fitness Essentials</h1>
                     <button class="bg-white py-1.5 px-5 rounded-full mt-8 text-black font-medium">Shop</button>
-                </div>
+                </article>
             </div>
         </div>
     </div>
@@ -143,99 +144,99 @@ const createWomenPage = () => {
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-1.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 2nd card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-2.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 3rd card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-3.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 4th card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-4.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 5th card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-5.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 6th card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-6.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 7th card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-7.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 8th card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-8.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
             <!-- 9th card image -->
             <div class="w-full">
               <div class="w-[330px] 2xl:w-[600px]">
                   <img class="w-full" src="../../images/women/popular-9.png">
-                  <div class="flex flex-col py-4 mb-8">
+                  <article class="flex flex-col py-4 mb-8">
                       <h1 class="text-sm font-semibold">Air Jordan 4 Retro "Industrial Blue"</h1>
                       <h1 class="text-gray-500 text-sm">Big Kids' Shoes</h1>
                       <h1 class="mt-2 text-sm font-medium">$160</h1>
-                  </div>
+                  </article>
               </div>
             </div>
         </div>
